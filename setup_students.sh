263898c8d7619fdc7cb85ec867e96d00eedf1118
odsogunro <dami.kazeem@gmail.com>
2016-05-30 12:25:24 -0400

    cleanups and updates

--- setup_students.sh
+++ setup_students.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # file processing
-file data/scores.txt data/schools.txt data/Demographics.txt
+file data/scores.txt data/schools.txt data/Demographics.txt 
 dos2unix data/scores.txt data/schools.txt data/Demographics.txt
 
 # using column command to resolve ^I tabs for sqlite3 load
@@ -10,7 +10,9 @@ column -n -t -s"  " data/scores.txt > data/scoresClean.txt
 column -n -t -s"  " data/schools.txt > data/schoolsClean.txt
 column -n -t -s"  " data/Demographics.txt > data/demographicsClean.txt
 
-cat -A data/scores.txt data/schools.txt data/Demographics.txt
+#cat -A data/scores.txt data/schools.txt data/Demographics.txt > /dev/null 2>&1
 
 # run program
-sqlite3 students.db -init createStudentsTables.sql 
+sqlite3 students.db -init createStudentsTable.sql 
+
+
