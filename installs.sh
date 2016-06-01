d3bbb861a88a5d2137ef59037f76185151c74492
odsogunro <dami.kazeem@gmail.com>
2016-06-01 03:06:35 -0400

    added provisions for vagrant

---
+++ installs.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+
+sudo apt-get install -y column >/dev/null 2>&1
+sudo apt-get install -y dos2unix >/dev/null 2>&1
+sudo apt-get install -y file >/dev/null 2>&1
+sudo apt-get install -y sqlite3 >/dev/null 2>&1
+sudo apt-get install -y tofrodos >/dev/null 2>&1
+
